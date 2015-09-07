536aa056ca01308f2bef6f9c3c9d7ce8c5245d7f
Student <student@RU-109.local>
2015-09-07 12:19:28 -0600

    3rd

---
+++ loops1.js
@@ -0,0 +1,35 @@
+// for (var i = 0; i < 501; i+=100){
+// 	console.log(i);
+// }
+
+// for (var i=1; i <65; i*=2){
+// 	console.log(i);
+// }
+
+// for (var i=1; i <4; i++){
+// 	console.log(i);
+// 	console.log(i);
+// 	console.log(i);
+// }
+
+// for (var i=0; i<11; i+=2){
+// 	console.log(i);
+// }
+
+// for (var i=3; i<16; i+=3){
+// 	console.log(i);
+// }
+
+// for (var i=9; i>=0; i--){
+// 	console.log(i);
+// }
+
+// for (var i=0; i<4; i++){
+// 	console.log(i);
+// }
+// for (var i=0; i<4; i++){
+// 	console.log(i);
+// }
+// for (var i=0; i<4; i++){
+// 	console.log(i);
+// }
